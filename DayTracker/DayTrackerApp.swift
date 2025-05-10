c322f64ab84b2fd280f0a5668a227bc25e280e7c
Alican <alicanozer60@gmail.com>
2025-05-10 12:14:27 -0400

    Initial Commit

---
+++ DayTracker/DayTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DayTrackerApp.swift
+//  DayTracker
+//
+//  Created by Alican Ozer on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DayTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
